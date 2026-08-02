62fe72cb1c0397e68f8e0025170622e793efb56b
Newis <sapablazaig@gmail.com>
2026-08-02 13:54:50 -0400

    limpieza: elimina duplicados de raiz y backend, backend corre desde package.json raiz

--- src/routes/categorias.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { asyncHandler } = require('../utils/asyncHandler');
-const ctrl = require('../controllers/categorias.controller');
-
-const router = Router();
-
-router.get('/', asyncHandler(ctrl.arbol));
-
-module.exports = router;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`Armonia backend escuchando en http://localhost:${PORT}`);
-});
