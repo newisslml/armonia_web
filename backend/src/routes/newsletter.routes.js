3f8ef938baf84c34687c251677d3aab890696846
TortugaDelSur <tortugadelsur91@gmail.com>
2026-08-03 01:32:43 -0400

    chore: quitar backend del repo, agregar a gitignore

--- backend/src/routes/newsletter.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { asyncHandler } = require('../utils/asyncHandler');
-const ctrl = require('../controllers/newsletter.controller');
-
-const router = Router();
-
-router.post('/', asyncHandler(ctrl.suscribir));
-
-module.exports = router;
